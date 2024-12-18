86f1e3d04bd30147dd7d118f26746f743b4eb7ce
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-12-18 13:35:10 +0100

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Suspense } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import Loader from "./components/Loader/Loader";
 
 function App() {
   return (
     <>
       <Navigation />
+      <main>
+        <Suspense fallback={<Loader />}></Suspense>
+      </main>
     </>
   );
 }
