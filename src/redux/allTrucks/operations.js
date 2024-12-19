3ee7e2fd7062188f8be498044cd336e5c6f4294a
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-12-20 00:45:08 +0100

    create operations file

--- src/redux/allTrucks/operations.js
+++ src/redux/allTrucks/operations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { instance } from "../API/server";
+import { selectFilters } from "../filterTrucks/selectors";
 
 export const fetchAllTrucks = createAsyncThunk(
   "trucks/fetchAllTrucks",
@@ -23,6 +24,27 @@ export const fetchTruckById = createAsyncThunk(
       return data;
     } catch (error) {
       thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const fetchFilteredTrucks = createAsyncThunk(
+  "trucks/fetchFilteredTrucks",
+  async (_, thunkApi) => {
+    const state = thunkApi.getState();
+    const filters = selectFilters(state);
+
+    try {
+      const { data } = await instance.get("/trucks", {
+        params: {
+          location: filters.location,
+          form: filters.form,
+          features: filters.features.join(","),
+        },
+      });
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
     }
   }
 );
